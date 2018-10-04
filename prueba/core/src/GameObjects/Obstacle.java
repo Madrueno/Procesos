192aa3084d69ae4a26c965de4cd37da7a2164a0e
sandruskyi <s.gomezgalv@alumnos.urjc.es>
2018-10-04 20:42:36 +0200

    Creada clase Obstacle
    Obstacle será cada pixel que en conjunto formará una barrera

---
+++ prueba/core/src/GameObjects/Obstacle.java
@@ -0,0 +1,42 @@
+package GameObjects;
+
+import com.badlogic.gdx.math.*;
+
+public class Obstacle { //Mi clase obstaculo es solo un pixel de cada barrera, despues habrá que crear la barrera entera
+                        //Lo he dividido así porque lo veo más fácil para programar más tarde la destrucción de la montaña tanto
+                        //por abajo como por arriba. Destruyendose así cada Obstacle poco a poco mediante status.
+
+    Vector2 position;
+    boolean status;
+
+    public Obstacle ( float x, float y){
+        this.position= new Vector2(x,y);
+        this.status=true;
+    }
+    public Obstacle (Vector2 v){
+        this.position= v;
+        this.status=true;
+    }
+
+    public Vector2 getPosition (){
+        return this.position;
+    }
+
+    public boolean getStatus (){
+        return this.status;
+    }
+.
+    public void setStatus(boolean s){
+        this.status = s;
+    }
+
+    public void setPosition (float x, float y){
+        this.position = (x,y);
+    }
+
+    public void setPositionV2(Vector2 v){
+        this.position= v;
+    }
+
+
+}
